a7f453d59a8854dab1dffdc276167e79ec00afed
Gaurang Raval <garaval@gmail.com>
2020-11-06 14:11:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+  res.send(`Hello ${target}!, it seems GH and GCP integrated with Github Actions and Workflow`);
 });
 
 const port = process.env.PORT || 8080;
